9f0f8fded86c0a59d0114722ef5d2c822fd7c755
Mariana Perna <marianaperna@ua.pt>
2023-11-04 19:26:53 +0000

    models feitos e display de todos os dados das receitas

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Receita,Categoria,Avaliacao
+from .models import Receita,Categoria,Avaliacao,Ingrediente,ReceitaIngrediente,Frigorifico,Favoritos,ReceitasListaCompras,ReceitasPlanificacao,CategoriaIngrediente
 
 admin.site.register(Receita)
 admin.site.register(Categoria)
-admin.site.register(Avaliacao)
-
+admin.site.register(CategoriaIngrediente)  
+admin.site.register(Avaliacao)                   
+admin.site.register(Ingrediente)  
+admin.site.register(ReceitaIngrediente)  
+admin.site.register(Frigorifico)  
+admin.site.register(Favoritos)  
+admin.site.register(ReceitasListaCompras)  
+admin.site.register(ReceitasPlanificacao)  
